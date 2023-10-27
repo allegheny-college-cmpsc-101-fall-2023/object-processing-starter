5cd09f722b60da6ffb0bb37ebce12b1425dc52d3
Emily Graber <egragraber@allegheny.edu>
2023-10-27 00:23:42 -0400

    Remove namedtuple

--- objectprocessor/objectprocessor/person.py
+++ objectprocessor/objectprocessor/person.py
@@ -1,39 +1,10 @@
 """Represent a person using an object-oriented approach."""
 
-import collections
-import itertools
 
 from typing import List
 
 # TODO: implement all of the required source code for the Person class
 # TODO: refer to the course slides on the course web site for more details!
-
-
-def create_constants(name, *args, **kwargs):
-    """Create a namedtuple of constants."""
-    # the constants are created such that:
-    # the name is the name of the namedtuple
-    # for *args with "Constant_Name" or **kwargs with Constant_Name = "AnyConstantName"
-    # note that this creates a constant that will
-    # throw an AttributeError when attempting to redefine
-    new_constants = collections.namedtuple(name, itertools.chain(args, kwargs.keys()))
-    return new_constants(*itertools.chain(args, kwargs.values()))
-
-
-# define the index locations for the person
-person_index = create_constants(
-    "person", Name=0, Country=1, Phone_Number=2, Job=3, Email=4
-)
-
-# define the names of the attributes for the person
-person_attribute = create_constants(
-    "person",
-    Name="name",
-    Country="country",
-    Phone_Number="phone_number",
-    Job="job",
-    Email="email",
-)
 
 
 class Person:
